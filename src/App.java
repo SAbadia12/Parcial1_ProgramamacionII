c9bffe0615e135ce630b2cf878eefe07d9e60f8e
Juan <juanalmendra3700@gmail.com>
2025-09-02 20:15:52 -0500

    Se creó el Objeto libro2

--- src/App.java
+++ src/App.java
@@ -7,5 +7,28 @@ public class App {
         libro1.prestamo();
         libro1.prestamo();
         libro1.imprimir();
+
+
+        Libro libro2 = new Libro("Los Pollitos Dicen", "Gerald Espinoza", 30, 17);
+    
+        libro2.imprimir();
+        libro2.prestamo();
+        libro2.prestamo();
+        libro2.prestamo();
+        libro2.prestamo();
+        libro2.prestamo();
+        libro2.prestamo();
+        libro2.prestamo();
+        libro2.imprimir();
+        libro2.devolucion();
+        libro2.devolucion();
+        libro2.devolucion();
+        libro2.imprimir();
+
+    
+    
+    
     }
+
+
 }
